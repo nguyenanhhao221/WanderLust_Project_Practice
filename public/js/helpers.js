0694ab5e3f72465f3c664cef2fa0404ecaaf1600
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-01-05 01:58:56 +0700

    add class for weather icon to work with css

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -3,5 +3,5 @@ const createWeatherHTML = currentDay => {
     return `<h2>${weekDays[(new Date).getDay()]}</h2>
             <h2>Temperature: ${currentDay.main.temp} &deg;C</h2>
             <h2>Condition: ${currentDay.weather[0].description}</h2>
-            <img src="http://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png" alt="">`;
+            <img class = "weathericon" src="http://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png" alt="">`;
 };
