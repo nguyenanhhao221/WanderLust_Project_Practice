83c5a2ce2db9f76060888d09c49341763bbcc241
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-01-05 02:03:22 +0700

    create helper function for HTML content for Place

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -6,3 +6,12 @@ const createWeatherHTML = currentDay => {
             <img class = "weathericon" src="http://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png" alt="">`;
 };
 
+const createPlacesHTML = (resultArr, index) => {
+    return `<h2>${resultArr[index].name}</h2>
+           <img class = "venueimage"src="${resultArr[index].categories[0].icon.prefix}bg_64${resultArr[index].categories[0].icon.suffix}" alt="icon" />
+           <h3>Address:</h3>
+           <p>${resultArr[index].location.address}</p> 
+           <p>${resultArr[index].location.locality}</p>
+           <p>${resultArr[index].location.admin_region}</p>
+           `;
+}
